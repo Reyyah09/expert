b470c94d7c73b7f5c116a6e4eac01730606c3150
smarttech143 <sardar.zubair19@yahoo.com>
2020-05-09 18:56:59 +0500

    Our project section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar';
 import Body from '../src/components/body/body';
+import Project from '../src/components/projects/project'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './asserts/style/style.scss';
 function App () {
   return ( 
     <div className="App">
       <Navbar/>
-     
+     <Project/>
       <Body/>
+      
       
     </div>
   );
--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 return(
 <>
-<div className="d-flex p-5 bg-secondary">
-<h2>Our Projects</h2>
+<div className="container mt-pb">
+<h2 className="clr-blue fnt-wt fnt-40 mb-25 ml-5">OUR PROJECTS</h2>
+<p className="ml-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+ Rem <br/> possimus distinctio ex.
+  Natus totam voluptatibus animi aspernatur <br/> ducimus quas
+   obcaecati mollitia quibusdam temporibus
+ culpa <br/>dolore molestias blanditiis consequuntur sunt nisi.</p>
 
 
 </div>
